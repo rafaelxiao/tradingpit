f230d3f9427396729938842ea64103c846b4f06b
ChickCake <owl@ChickCakes-MacBook-Air.local>
2023-01-02 18:15:46 +0800

    using https

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         createProxyMiddleware('/api', {
-            target: 'http://api.tushare.pro',
+            target: 'https://api.tushare.pro',
             // target: 'http://www.baidu.com',
             changeOrigin: true,
             pathRewrite: {
